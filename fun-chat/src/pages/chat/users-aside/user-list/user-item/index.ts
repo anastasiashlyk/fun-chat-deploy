74587cf510d41630508b8fb9b9980025eb3f692a
Anastasia Shlyk <shlykk.anastasia@gmail.com>
2026-02-12 00:48:22 +0100

    commit

--- fun-chat/src/pages/chat/users-aside/user-list/user-item/index.ts
+++ fun-chat/src/pages/chat/users-aside/user-list/user-item/index.ts
@@ -1,17 +1,9 @@
 import { BaseComponent } from '@/core/base-component';
 
-import { TextElement } from '@/components/text-element';
-
 export class UserItem extends BaseComponent<HTMLLIElement> {
-  private unreadCount: TextElement;
-
   constructor(username: string, isLogined: boolean) {
     super('li', { className: 'user-item' });
-
-    this.append(new TextElement({ tag: 'span', text: username }));
-    this.unreadCount = new TextElement({ tag: 'span', text: '0', className: 'unread-count' });
-    this.append(this.unreadCount);
-    this.unreadCount.hide();
+    this.element.textContent = username;
 
     if (isLogined) {
       this.element.classList.add('active');
